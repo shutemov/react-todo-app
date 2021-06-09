9400ba615d034915c18118635be270124b4b16a2
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:33:09 +0300

    Добавляет TodoManager конструктор

--- src/components/TodoManager.js
+++ src/components/TodoManager.js
@@ -3,5 +3,16 @@ import TodoList from "./TodoList";
 import TodoDoneList from "./TodoDoneList";
 
 class TodoManager extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.switchDoneTodo = this.switchDoneTodo.bind(this);
+  }
 }
 export default TodoManager;
