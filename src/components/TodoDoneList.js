2504d31629a55ef8620198401c4f71fdb328ee03
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:45:56 +0300

    Создает компонент TodoDoneList
    Отображает Todo с состоянием isDone===true

---
+++ src/components/TodoDoneList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import TodoListViewer from "./TodoListViewer";
+import Todo from "./Todo";
+
+class TodoDoneList extends React.Component {
+}
+export default TodoDoneList;
