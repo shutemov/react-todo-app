d9f92b36af012d27f6f24ecd089438e407c25cb8
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:34:07 +0300

    Добавляет функции для управления Todo

--- src/components/TodoManager.js
+++ src/components/TodoManager.js
@@ -14,5 +14,45 @@ class TodoManager extends React.Component {
     this.deleteTodo = this.deleteTodo.bind(this);
     this.switchDoneTodo = this.switchDoneTodo.bind(this);
   }
+
+  addTodo() {
+    const name = document.querySelector("input").value;
+    const isDone = false;
+    const newTodo = {
+      name,
+      isDone,
+    };
+
+    let todos = [...this.state.todos, newTodo];
+
+    this.setState({ todos });
+  }
+
+  deleteTodo(e) {
+    const deleteCandidateElement = e.target.closest("li");
+    const deleteCandidateTodoText =
+      deleteCandidateElement.getAttribute("data-todo-text");
+
+    let todos = this.state.todos.filter((todo) => {
+      return todo.name != deleteCandidateTodoText;
+    });
+
+    this.setState({ todos });
+  }
+
+  switchDoneTodo(e) {
+    const targetElement = e.target.closest("li");
+    const targetTodoName = targetElement.getAttribute("data-todo-text");
+
+    const todos = this.state.todos.map((todo) => {
+      if (todo.name === targetTodoName) {
+        todo.isDone = !todo.isDone;
+      }
+
+      return todo;
+    });
+
+    this.setState({ todos });
+  }
 }
 export default TodoManager;
