caad84c993272fdd2dfde42b239585fdafe40a71
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:44:54 +0300

    Добавляет TodoList рендер функцию

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,6 +13,34 @@ class TodoList extends React.Component {
       return !todo.isDone;
     });
   }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        <input placeholder="Add todo" />
+        <button onClick={this.props.todoAdder}> + </button>
+        <hr />
+        <TodoListViewer>
+          <ol>
+            {this.getUndoneTodos().map(({ name, isDone }, index) => {
+              console.log("todo in map", name, isDone);
+
+              return (
+                <Todo
+                  name={name}
+                  todoDoner={this.props.todoDoner}
+                  todoDeleter={this.props.todoDeleter}
+                  isDone={isDone}
+                  key={index}
+                />
+              );
+            })}
+          </ol>
+        </TodoListViewer>
+      </div>
+    );
+  }
 }
 
 export default TodoList;
