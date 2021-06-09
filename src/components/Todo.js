36e7da5dac225316c6c2c8868dc0b481e1373cf0
Shutemov <shutemov1997@mail.ru>
2021-06-09 16:31:32 +0300

    Чистит код Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,19 @@
-import React from "react"
+import React from "react";
 
-class Todo extends React.Component{
-    constructor(props){
-        super(props)
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-        this.state = {
-            isDone: false,
-            isArchived: false
-        }
-
-        this.switchDone = this.switchDone.bind(this)
-    }
-
-    switchDone(){       
-        this.setState({isDone:!this.state.isDone})
-    }
-
-
-    render(){
-        return (
-            <li data-todo-text={this.props.name}>
-                <h3> {this.props.name} </h3>
-                <button onClick={this.props.todoDoner}>Done</button>
-                <button onClick={this.props.todoDeleter}>Delete</button>
-            </li>
-        )
-    }
+  render() {
+    return (
+      <li data-todo-text={this.props.name}>
+        <h3> {this.props.name} </h3>
+        <button onClick={this.props.todoDoner}>Done</button>
+        <button onClick={this.props.todoDeleter}>Delete</button>
+      </li>
+    );
+  }
 }
 
-export default Todo
+export default Todo;
