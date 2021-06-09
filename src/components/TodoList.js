19d262129f4aee709bb6cb8e3a22616241a28bd9
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:43:49 +0300

    Добавляет TodoList конструктор

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,12 @@
+import React from "react";
+import Todo from "./Todo";
+import TodoListViewer from "./TodoListViewer";
 
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
 }
 
+export default TodoList;
