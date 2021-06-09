0d0c492a6e96bca6c49ec9af5bed82c5c1ef9bc5
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:49:11 +0300

    Добавляет TodoListViewer конструктор и рендер

--- src/components/TodoListViewer.js
+++ src/components/TodoListViewer.js
@@ -2,6 +2,17 @@ import React from 'react'
 import Todo from './Todo'
 
 class TodoListViewer extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
 }
 
 export default TodoListViewer
