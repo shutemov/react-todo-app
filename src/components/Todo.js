6c9ea143327238fa2d9464135035491e1834766f
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:40:05 +0300

    Обновляет пропсы и наименование дата атрибута

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,10 +19,10 @@ class Todo extends React.Component{
 
     render(){
         return (
-            <li data-attr={this.props.text}>
-                <h3> {this.props.text} </h3>
-                <button onClick={this.switchDone}>{this.state.isDone?'undone':'done'}</button>
-                <button onClick={this.props.onDelete}>Delete</button>
+            <li data-todo-text={this.props.name}>
+                <h3> {this.props.name} </h3>
+                <button onClick={this.props.todoDoner}>Done</button>
+                <button onClick={this.props.todoDeleter}>Delete</button>
             </li>
         )
     }
