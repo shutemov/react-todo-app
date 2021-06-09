9a7858fb2a95154289ddd68f5edf3ca9b8be7cd9
Shutemov <shutemov1997@mail.ru>
2021-06-09 15:32:18 +0300

    Создает компонент TodoManger

---
+++ src/components/TodoManager.js
@@ -0,0 +1,7 @@
+import React from "react";
+import TodoList from "./TodoList";
+import TodoDoneList from "./TodoDoneList";
+
+class TodoManager extends React.Component {
+}
+export default TodoManager;
