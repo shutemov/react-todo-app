606f70b94b465f2cca7dee7ced94562073a6c7e0
Shutemov <shutemov1997@mail.ru>
2021-06-11 12:53:27 +0300

    Добавляет конструктор в PadManger
    С мок данными пэдов

--- src/components/PadManager.js
+++ src/components/PadManager.js
@@ -3,6 +3,30 @@ import PadAdder from "./PadAdder";
 import PadList from "./PadList";
 
 class PadManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pads: [
+        {
+          id: 1,
+          title: "pad1",
+        },
+        {
+          id: 2,
+          title: "pad2",
+        },
+        {
+          id: 3,
+          title: "pad3",
+        },
+        {
+          id: 4,
+          title: "pad4",
+        },
+      ],
+    };
+  }
+
 }
 
 export default PadManager;
