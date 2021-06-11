a545a948966c0c843fc4f1fab5594ec765086425
Shutemov <shutemov1997@mail.ru>
2021-06-11 12:56:53 +0300

    Переименовывает компонент Main в MainPage

---
+++ src/components/MainPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppLogo from './AppLogo'
+import TodoManager from './TodoManager'
+import PadManager from './PadManager'
+
+class MainPage extends React.Component{
+}
+
+export default MainPage
