bbea08845d0458ead57e6555f69b467de371ac86
Shutemov <shutemov1997@mail.ru>
2021-06-11 13:04:57 +0300

    Реализует route зависимости в App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import MainPage from "./components/MainPage";
+import PadPage from "./components/PadPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
-      {/* <TodoList/> */}
-    </div>
+    <Router>
+      <div className="App"></div>
+
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route path="/pads/:id">
+          <PadPage />
+        </Route>
+        <Route path="*">404</Route>
+      </Switch>
+    </Router>
   );
 }
 
