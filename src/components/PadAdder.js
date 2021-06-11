a6ea38770356eabaf2ee13dfdd9962471a0149b1
Shutemov <shutemov1997@mail.ru>
2021-06-11 12:46:52 +0300

    Добавляет конструктор в PadAdder

--- src/components/PadAdder.js
+++ src/components/PadAdder.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class PadAdder extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isCreating: false,
+    };
+
+    this.switchCreatingMode = this.switchCreatingMode.bind(this);
+  }
+
 }
 
 export default PadAdder;
