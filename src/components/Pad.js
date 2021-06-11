573c1185d93e5b4c9ed2c7ff7055329f42169de3
Shutemov <shutemov1997@mail.ru>
2021-06-11 12:45:23 +0300

    Создает компонент Pad
    Хранит список задач

---
+++ src/components/Pad.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+class Pad extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to={`/pads/${this.props.id}`}>{this.props.title}</Link>
+        <button>Delete</button>
+      </div>
+    );
+  }
+}
+
+export default Pad;
