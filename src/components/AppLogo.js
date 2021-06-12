07f2ec1de0bb9d87012557358a8235da9fa7585c
Shutemov <shutemov1997@mail.ru>
2021-06-12 19:59:26 +0300

    Обновляет рендер Logo
    Текстовое лого стало рисунком <picture>

--- src/components/AppLogo.js
+++ src/components/AppLogo.js
@@ -1,5 +1,16 @@
-function Logo(){
-    return (<h1>TODO PAD</h1>)
+import React from "react";
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <div className="logo">
+        <picture>
+          <source srcSet="Todo_Pad.png" />
+          <img src="Todo_Pad.png" alt="Todo Pad" />
+        </picture>
+      </div>
+    );
+  }
 }
 
-export default Logo
+export default Logo;
