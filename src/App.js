610925530bb53168e9ed5d55235e7c5978ee0a2b
Shutemov <shutemov1997@mail.ru>
2021-06-12 20:02:02 +0300

    Минорное автоформатирование

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <div className="App"></div>
-
-      <Switch>
-        <Route exact path="/">
-          <MainPage />
-        </Route>
-        <Route path="/pads/:id">
-          <PadPage />
-        </Route>
-        <Route path="*">404</Route>
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route path="/pads/:id">
+            <PadPage />
+          </Route>
+          <Route path="*">404</Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
