c52b2a786885901a59d5ec1c3868d68abfba9261
Shutemov <shutemov1997@mail.ru>
2021-06-15 16:42:33 +0300

    Удаляет мок данные из состояния PadManager

--- src/components/PadManager.js
+++ src/components/PadManager.js
@@ -5,33 +5,9 @@ import PadList from "./PadList";
 class PadManager extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      pads: [
-        {
-          id: 1,
-          title: "pad1",
-        },
-        {
-          id: 2,
-          title: "pad2",
-        },
-        {
-          id: 3,
-          title: "pad3",
-        },
-        {
-          id: 4,
-          title: "pad4",
-        },
-        {
-          id: 5,
-          title: "pad3",
-        },
-        {
-          id: 6,
-          title: "pad4",
-        },
-      ],
+      pads: [],
     };
   }
 
