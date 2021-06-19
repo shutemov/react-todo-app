5245e3521b605e786e7889552ba255ed34493d47
Shutemov <shutemov1997@mail.ru>
2021-06-19 10:18:23 +0300

    Обновляет рендер TodoAdder

--- src/components/TodoAdder.js
+++ src/components/TodoAdder.js
@@ -3,9 +3,14 @@ import React from "react";
 class TodoAdder extends React.Component {
   render() {
     return (
-      <div>
-        <input placeholder="Add todo" />
-        <button onClick={this.props.todoAdder}> + </button>
+      <div className="pad-page__todo-adder-wrapper">
+        <input className="pad-page__add-todo-input" placeholder="Add todo" />
+        <button
+          className="pad-page__add-todo-button"
+          onClick={this.props.todoAdder}
+        >
+          +
+        </button>
       </div>
     );
   }
