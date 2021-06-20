59abde55ec07ff2d2dd36aed7d2e2db2c5a89345
Shutemov <shutemov1997@mail.ru>
2021-06-20 12:44:05 +0300

    Применяет новые стили к Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <li className="pad-page__todo" data-todo-text={this.props.name}>
-        <h3> {this.props.name} </h3>
-        <div className="pad-page__todo-button-wrapper">
-          <button onClick={this.props.todoDeleter}>X</button>
-          <button onClick={this.props.todoDoner}>D</button>
+        <div className="pad-page__li-marker" />
+        <div className="pad-page__li-body">
+          <div className="pad-page__todo-text"> {this.props.name} </div>
+          <div className="pad-page__todo-button-wrapper">
+            <button onClick={this.props.todoDeleter}>X</button>
+            <button onClick={this.props.todoDoner}>D</button>
+          </div>
         </div>
       </li>
     );
