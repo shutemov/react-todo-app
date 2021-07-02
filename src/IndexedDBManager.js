860079b1f89f3ad655bceae6bf446ab34ececd66
Shutemov <shutemov1997@mail.ru>
2021-07-02 12:38:37 +0300

    Реализует init() в IndexedDBManager

--- src/IndexedDBManager.js
+++ src/IndexedDBManager.js
@@ -3,6 +3,15 @@ class IndexedDBManager {
     this.dbName = dbName;
     this.storeName = storeName;
   }
+
+  async init() {
+    try {
+      await this._openConnection();
+      await this._createPadStore(); //sync operation
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 export default IndexedDBManager;
