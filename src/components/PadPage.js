f5b9d0cdb3bcaf6aedfb7aff9e554e51ba299749
Shutemov <shutemov1997@mail.ru>
2021-07-05 20:05:26 +0300

    Обновляет конструктор PadPage

--- src/components/PadPage.js
+++ src/components/PadPage.js
@@ -1,10 +1,16 @@
 import React from "react";
 import TodoManager from "./TodoManager";
 import BackToMainButton from "./BackToMainLogo";
-import IndexedDBManager from "../IndexedDBManager";
 import { withRouter } from "react-router-dom";
 
 class PadPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      padId: undefined,
+    };
+  }
+
   render() {
     return (
       <div className="pad-page">
