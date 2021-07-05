7ac3cc2ee24d9adc9ea138e530a8f28e90292631
Shutemov <shutemov1997@mail.ru>
2021-07-05 20:16:46 +0300

    Обновляет рендер PadList

--- src/components/PadList.js
+++ src/components/PadList.js
@@ -4,7 +4,13 @@ import Pad from "./Pad";
 class PadList extends React.Component {
   getPadsTemplate() {
     const mappedPads = this.props.pads.map(({ id, title }) => (
-      <Pad id={id} title={title} key={id} padDelete={this.props.padDelete} />
+      <Pad
+        id={id}
+        title={title}
+        key={id}
+        deletePad={this.props.deletePad}
+        updatePad={this.props.updatePad}
+      />
     ));
 
     return mappedPads;
