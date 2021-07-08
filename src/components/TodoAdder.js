34c3434b1b5c689b7d6a2a2485d22cb00a9241d7
Shutemov <shutemov1997@mail.ru>
2021-07-08 10:39:17 +0300

    Деструктурирует пропс в TodoAdder

--- src/components/TodoAdder.js
+++ src/components/TodoAdder.js
@@ -2,12 +2,14 @@ import React from "react";
 
 class TodoAdder extends React.Component {
   render() {
+    const {addTodo} = this.props
+    
     return (
       <div className="pad-page__todo-adder-wrapper">
         <input className="pad-page__add-todo-input" placeholder="Add todo" />
         <button
           className="pad-page__add-todo-button"
-          onClick={this.props.todoAdder}
+          onClick={addTodo}
         >
           +
         </button>
