07d2a6a7e328d2fc725f6405d7834d7f3c9a1433
Shutemov <shutemov1997@mail.ru>
2021-07-09 12:06:05 +0300

    Переводит на StyledComponents TodoDoneList

--- src/components/TodoDoneList.js
+++ src/components/TodoDoneList.js
@@ -1,6 +1,36 @@
 import React from "react";
-import TodoListViewer from "./TodoListViewer";
+import styled from "styled-components";
+import TodosViewer from "./TodoListViewer";
 import DoneTodo from "./DoneTodo";
+
+const StyledTodoDoneList = styled.div`
+  position: relative; /*for absolute position TodoAdder*/
+  width: 375px;
+  height: 500px;
+  border: 1px solid;
+  overflow: hidden;
+`;
+
+const Header = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  padding-top: 30px;
+  padding-left: 40px;
+`;
+
+const Title = styled.div`
+  font-size: 20pt;
+`;
+
+const ClearButton = styled.div`
+  border-radius: 40px;
+  width: 100px;
+  height: 40px;
+  background-color: #c4c4c4;
+  border: none;
+  margin-left: 30px;
+`;
 
 class TodoDoneList extends React.Component {
   constructor(props) {
@@ -34,19 +64,17 @@ class TodoDoneList extends React.Component {
 
   render() {
     const { title, undoneAll } = this.props;
-    
+
     return (
-      <div className="todo-done-list">
-        <div className="todo-done-list__header">
-          <div className="todo-done-list__title">{title}</div>
-          <button className="todo-done-list__drop-button" onClick={undoneAll}>
-            Очистить
-          </button>
-        </div>
-        <TodoListViewer>
+      <StyledTodoDoneList>
+        <Header>
+          <Title>{title}</Title>
+          <ClearButton onClick={undoneAll}>Очистить</ClearButton>
+        </Header>
+        <TodosViewer>
           <ol>{this.getDoneTodosTemplate()}</ol>
-        </TodoListViewer>
-      </div>
+        </TodosViewer>
+      </StyledTodoDoneList>
     );
   }
 }
