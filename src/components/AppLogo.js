9d24213693149647216e41caa53df3af37944469
Shutemov <shutemov1997@mail.ru>
2021-07-09 12:04:06 +0300

    Переводит на StyledComponents AppLogo

--- src/components/AppLogo.js
+++ src/components/AppLogo.js
@@ -1,14 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledLogo = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: center;
+`;
 
 class Logo extends React.Component {
   render() {
     return (
-      <div className="logo">
+      <StyledLogo>
         <picture>
           <source srcSet="todo-pad-logo.png" />
           <img src="todo-pad-logo.png" alt="Todo Pad" />
         </picture>
-      </div>
+      </StyledLogo>
     );
   }
 }
