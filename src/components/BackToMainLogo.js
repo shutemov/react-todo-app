9f309d7c8a9b89e7cbf8250f2b7f0fa38b1a38b3
Shutemov <shutemov1997@mail.ru>
2021-07-09 12:04:21 +0300

    Переводит на StyledComponents BackToMainLogo

--- src/components/BackToMainLogo.js
+++ src/components/BackToMainLogo.js
@@ -1,15 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+
+const StyledLink = styled(Link)`
+  font-size: 3em;
+`;
 
 class BackToMainButton extends React.Component {
   render() {
     return (
-      <Link className="pad-page__back-to-main-link" to="/">
+      <StyledLink to="/">
         <picture>
           <source srcSet="../todo-pad-back.png" />
           <img src="todo-pad-back.png" alt="Todo Pad" />
         </picture>
-      </Link>
+      </StyledLink>
     );
   }
 }
