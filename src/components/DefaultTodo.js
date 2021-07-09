367486137c6528e7102dd41a6e3bf58685c65b58
Shutemov <shutemov1997@mail.ru>
2021-07-09 12:04:33 +0300

    Переводит на StyledComponents DefaultTodo

--- src/components/DefaultTodo.js
+++ src/components/DefaultTodo.js
@@ -1,24 +1,72 @@
 import React from "react";
+import styled from "styled-components";
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
+  background-color: #c4c4c4;
+  border-radius: 30px;
+  width: 100%;
+  min-height: 50px;
+  margin-top: 12px;
+`;
+
+const LiMarker = styled.div`
+  display: list-item;
+  width: 10%;
+`;
+
+const LiBody = styled.div`
+  display: flex;
+  width: 90%;
+  align-items: center;
+  justify-content: start;
+  text-align: start;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif☺☺;
+`;
+
+const Text = styled.div`
+  width: 75%;
+  word-wrap: break-word;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 25%;
+`;
+
+const DoneButton = styled.button`
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  border: none;
+  background-color: #f3f3f5;
+`;
+
+const DeleteButton = styled.button`
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  border: none;
+  background-color: #edd0d0;
+`;
 
 class DefaultTodo extends React.Component {
   render() {
-    const { textContent, deleteTodo, doneTodo } = this.props;
+    const { name, deleteTodo, doneTodo } = this.props;
 
     return (
-      <li className="todo" data-todo-text={textContent}>
-        <div className="todo__li-marker" />
-        <div className="todo__li-body">
-          <div className="todo__text"> {textContent} </div>
-          <div className="todo__button-wrapper">
-            <button className="todo__done-button" onClick={deleteTodo}>
-              🧺
-            </button>
-            <button className="todo__delete-button" onClick={doneTodo}>
-              ✔️
-            </button>
-          </div>
-        </div>
-      </li>
+      <Li data-todo-text={name}>
+        <LiMarker />
+        <LiBody>
+          <Text> {name} </Text>
+          <ButtonContainer>
+            <DeleteButton onClick={deleteTodo}>🧺</DeleteButton>
+            <DoneButton onClick={doneTodo}>✔️</DoneButton>
+          </ButtonContainer>
+        </LiBody>
+      </Li>
     );
   }
 }
