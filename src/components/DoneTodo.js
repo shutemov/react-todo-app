f72ad33354d8c03096cc4437447957aade4879b7
Shutemov <shutemov1997@mail.ru>
2021-07-09 12:04:43 +0300

    Переводит на StyledComponents DoneTodo

--- src/components/DoneTodo.js
+++ src/components/DoneTodo.js
@@ -1,20 +1,64 @@
 import React from "react";
+import styled from "styled-components";
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
+  background-color: #c4c4c4;
+  border-radius: 30px;
+  width: 100%;
+  min-height: 50px;
+  margin-top: 12px;
+`;
+
+const LiMarker = styled.div`
+  display: list-item;
+  width: 10%;
+`;
+
+const LiBody = styled.div`
+  display: flex;
+  width: 90%;
+  align-items: center;
+  justify-content: start;
+  text-align: start;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif☺☺;
+`;
+
+const Text = styled.div`
+  width: 75%;
+  word-wrap: break-word;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 25%;
+`;
+
+const UndoneButton = styled.button`
+  width: 32px;
+  height: 32px;
+  border-radius: 40px;
+  background-color: #d7e9de;
+  border: none;
+  margin-left: 40%;
+`;
 
 class DoneTodo extends React.Component {
   render() {
+    const { switchDoneTodo, name } = this.props;
+
     return (
-      <li className="done-todo" data-todo-text={this.props.name}>
-        <div className="done-todo__li-marker" />
-        <div className="done-todo__li-body">
-          <div className="done-todo__text"> {this.props.name} </div>
-          <div className="done-todo__button-wrapper">
-            <button
-              className="done-todo__switch-button"
-              onClick={this.props.switchDoneTodo}
-            ></button>
-          </div>
-        </div>
-      </li>
+      <Li data-todo-text={name}>
+        <LiMarker />
+        <LiBody>
+          <Text> {name} </Text>
+          <ButtonContainer>
+            <UndoneButton onClick={switchDoneTodo} />
+          </ButtonContainer>
+        </LiBody>
+      </Li>
     );
   }
 }
